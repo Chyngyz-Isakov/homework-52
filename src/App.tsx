130496af5c441af48856bb0e2de396dbe92ad6de
Chyngyz Isakov <ellegantus@gmail.com>
2023-05-19 11:48:57 +0600

    create component CardView

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import './cards.css';
 import './App.css';
+import CardView from "./СardView/СardView";
 
-function App() {
-  return (
+const App = () => (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <CardView rank="K" suit="Diams"/>
     </div>
-  );
-}
+);
 
 export default App;
