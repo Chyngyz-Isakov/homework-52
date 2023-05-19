289462fe09a1c2046794418b3d78adf1c5be798e
Chyngyz Isakov <ellegantus@gmail.com>
2023-05-19 12:36:42 +0600

    create component CardView

---
+++ src/lib/CardDeck.ts
@@ -0,0 +1,18 @@
+import Card from "./Card";
+
+class CardDeck {
+    private readonly ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+    private readonly suits = ['clubs', 'diams', 'hearts', 'spades']
+    public deck: Card[] = [];
+
+    constructor() {
+        this.suits.forEach(suit => {
+            this.ranks.forEach(rank => {
+                const card = new Card(rank, suit);
+                this.deck.push(card);
+            })
+        })
+    }
+}
+
+export default CardDeck;
