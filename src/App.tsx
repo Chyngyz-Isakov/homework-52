ce1a5e3dcb9f66d73118e77d0edace733209be80
Chyngyz Isakov <ellegantus@gmail.com>
2023-05-22 15:40:59 +0600

    made pokerHand with check create method getOutcome

--- src/App.tsx
+++ src/App.tsx
@@ -6,35 +6,42 @@ import './cards.css';
 import './App.css';
 import PokerHand from "./lib/PokerHand";
 
+
+
+
 const App = () => {
     const [cards, setCards] = useState<Card []>([]);
 
+    const pokerHand = new PokerHand(cards);
+    let pokerHandText:string = pokerHand.getOutcome();
+
     const getCards = () => {
-        const cardDeck = new CardDeck()
+        const cardDeck = new CardDeck();
         const newCards = cardDeck.getCards(5);
         setCards(newCards);
+        // const pokerHand = new PokerHand(newCards);
+        // console.log(pokerHand.getOutcome());
     }
+
 
     if (cards.length === 0) {
         return <button onClick={getCards}>Dealing cards</button>
     }
 
-    const pokerHand = new PokerHand(cards);
-    console.log(pokerHand.getOutcome());
 
     return (
         <div className="App">
             <button onClick={getCards}>Dealing cards</button>
             <div className="cards">
                 {cards.map((card) => (
-                    <CardView key={card.rank + card.suit} rank={card.rank} suit={card.suit} />
+                    <CardView key={card.rank + card.suit} rank={card.rank} suit={card.suit}/>
                 ))}
-                {/*<CardView rank={fiveRandom[0].rank} suit={fiveRandom[0].suit}/>*/}
-                {/*<CardView rank={fiveRandom[1].rank} suit={fiveRandom[1].suit}/>*/}
-                {/*<CardView rank={fiveRandom[2].rank} suit={fiveRandom[2].suit}/>*/}
-                {/*<CardView rank={fiveRandom[3].rank} suit={fiveRandom[3].suit}/>*/}
-                {/*<CardView rank={fiveRandom[4].rank} suit={fiveRandom[4].suit}/>*/}
+
             </div>
+
+            <h1>Poker Hand :</h1>
+            <p><b>{pokerHandText}</b></p>
+
         </div>
     );
 };
--- src/lib/PokerHand.ts
+++ src/lib/PokerHand.ts
@@ -1,19 +1,41 @@
 import Card from "./Card";
 
 class PokerHand {
-    constructor(public cards:Card[]) {
+    constructor(public cards: Card[]) {
     };
 
     getOutcome() {
-        // const cards = this.cards
+        let count = 0;
+        let suits = 0;
 
+        let pokerHandShow = '';
 
+        for (let i = 0; i < this.cards.length; i++) {
+            for (let j = 0; j < this.cards.length; j++) {
+                if (this.cards[i].rank === this.cards[j].rank && this.cards[i].suit !== this.cards[j].suit) {
+                    count++;
+                }
+                if (this.cards[i].suit === this.cards[j].suit && this.cards[i].rank !== this.cards[j].rank){
+                    suits++;
+                }
+            }
+        }
 
+        if (suits === 20){
+            pokerHandShow = 'Flush';
+        }
 
-        return this.cards;
+        if (count === 2) {
+            pokerHandShow = 'one pair';
+        } else if (count === 6) {
+            pokerHandShow = 'three of a kind';
+        } else if (count === 4) {
+            pokerHandShow = 'two pairs';
+        } else {
+            pokerHandShow = 'no combo';
+        }
+        return pokerHandShow;
     }
-
-
 }
 
 export default PokerHand;
